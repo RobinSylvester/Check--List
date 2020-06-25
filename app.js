41a8c14fcc85af150ca66c7362c4e9f14aacef4b
RobinSylvester <46421233+RobinSylvester@users.noreply.github.com>
2020-06-26 00:14:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://robinsylvester:jaga1966@cluster0-b1zaz.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(" ", {useNewUrlParser: true, useUnifiedTopology: true });
 
 const itemsSchema = {
   name: String
